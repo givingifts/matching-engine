492c1212ca177d07042eb6693ad423be67c2feaf
Uriel Salischiker <uriel@urielsalis.me>
2021-10-18 19:14:41 +0200

    Revert "Fix dropped metric"

--- src/main/kotlin/gifts/givin/matching/common/stages/DoChecks.kt
+++ src/main/kotlin/gifts/givin/matching/common/stages/DoChecks.kt
@@ -25,10 +25,10 @@ object DoChecks : Stage<Unit> {
             MatchesTable.select { MatchesTable.sendTo eq MatchesTable.receiveFrom }.count()
         }
         val usersWithoutSendTo = transaction {
-            MatchesTable.select { MatchesTable.sendTo.isNull() and (MatchesTable.isDropped eq false) }.count()
+            MatchesTable.select { MatchesTable.sendTo.isNull() }.count()
         }
         val usersWithoutReceiveFrom = transaction {
-            MatchesTable.select { MatchesTable.receiveFrom.isNull() and (MatchesTable.isDropped eq false) }.count()
+            MatchesTable.select { MatchesTable.receiveFrom.isNull() }.count()
         }
         val totalUsers = transaction {
             MatchesTable.selectAll().count()
